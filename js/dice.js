a9db63f1fdc69937474a3de668dbb8d9cdd3f9b8
Andr3aBlanco <blancoblancoa@student.douglascollege.ca>
2022-04-26 20:44:32 -0700

    added total scores
    Keeps scores and allows to reset

--- js/dice.js
+++ js/dice.js
@@ -3,6 +3,12 @@ const output = document.querySelector('.output');
 const py1 = document.querySelector('#player1');
 const py2 = document.querySelector('#player2');
 
+const py1Total = document.getElementById('score1');
+const py2Total = document.getElementById('score2');
+
+let py1Tot = 0;
+let py2Tot = 0;
+
 //Event for button 
 btn.addEventListener('click', () => {
 
@@ -24,7 +30,13 @@ btn.addEventListener('click', () => {
     }
     output.innerHTML = result;
 
+    // Keep scores 
+    py1Tot += rolls[0];
+    py2Tot += rolls[1];
 
+    // displaying
+    py1Total.innerHTML = py1Tot;
+    py2Total.innerHTML = py2Tot;
 });
 
 //function for random value of dice 
@@ -59,6 +71,21 @@ function displayDice(score,element) {
 
     var diceHTML = '<img src="./img/' + diceImg + '.png" alt="Dice image"></img>';
     element.innerHTML = diceHTML;
+
 }
+
+function ResetMatch(){
+        // Keep scores 
+        py1Tot = 0
+        py2Tot = 0;
+    
+        // displaying
+        py1Total.innerHTML = 0;
+        py2Total.innerHTML = 0;
+        output.innerHTML = "";
+}
+
+
+// Keeping scores 
 
 
